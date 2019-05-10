4d26d0d3f7a58eb3ec10fadb842aeb21cd50beb7
Edward Levin <elevin14@gmail.com>
2019-05-10 15:08:52 -0700

    Finish section 2.2.3 part 2

--- sicp/2-2-3.scm
+++ sicp/2-2-3.scm
@@ -15,6 +15,40 @@
         (else (append (enumerate-tree (car tree))
                       (enumerate-tree (cdr tree))))))
 
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (square n) (* n n))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
 ; Exercise 2.33
 (define (map2 p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
@@ -101,3 +135,30 @@
 
 (reverse (list 1 2 3))
 (reverse2 (list 1 2 3))
+
+; Exercise 2.40
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(unique-pairs 5)
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(prime-sum-pairs 5)
+
+; Exercise 2.41
+
+(define (unique-triples n)
+  (flatmap (lambda (pair)
+             (map (lambda (k) (append pair (list k)))
+                  (enumerate-interval 1 (- (cadr pair) 1))))
+             (unique-pairs n)))
+
+(unique-triples 5)
+
+; Exercise 2.42
